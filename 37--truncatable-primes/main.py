4c59c4f479630078b6215d1c14afdecfeb5e5522
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-05-19 14:54:08 -0700

    Finish by using Sieve of Eratosthenes

--- 37--truncatable-primes/main.py
+++ 37--truncatable-primes/main.py
@@ -1,44 +1,56 @@
+from collections import OrderedDict
+
 def main():
-    n = 1
+    max_n = 1_000_000
+    primes = primes_up_to(max_n)
     found = []
-    while len(found) < 11:
-        if is_left_truncatable(n) and is_right_truncatable(n):
-            found.append(n)
-            print(f'Found bidirectionally-truncatable prime #{len(found)}: {n}')
-        if n % 1000 == 0:
-            print('...', n)
-        n += 1
-    print(sum(found))
+    for p in primes:
+        if is_left_truncatable(p, primes) and is_right_truncatable(p, primes):
+            found.append(p)
+            print(f'Found bidirectionally-truncatable prime #{len(found)}: {p}')
+    print(len(found), found)
 
 
-def is_prime(n):
-    if n <= 1:
-        return False
-    for m in range(2, n):
-        if n % m == 0:
-            return False
-    return True
+# A simple implementation of the Sieve of Eratosthenes.
+# n is inclusive
+def primes_up_to(n):
+    is_prime = OrderedDict((n, True) for n in range(2, n+1))
+    i = 2
+    while i <= n ** 0.5:
+        for j in range(i*2, n+1, i):
+            is_prime[j] = False
 
-def is_left_truncatable(p):
+        # increment
+        i += 1
+        while not is_prime[i]:
+            i += 1
+
+    result = set()
+    for n in is_prime:
+        if is_prime[n]:
+            result.add(n)
+    return result
+
+def is_left_truncatable(p, primes):
     if p < 10:
         return False
     p = str(p)
     for i in range(0, len(p)):
-        if not is_prime(int(p[i:])):
+        if int(p[i:]) not in primes:
             return False
     return True
 
-def is_right_truncatable(p):
+def is_right_truncatable(p, primes):
     if p < 10:
         return False
-    if not is_prime(p):
+    if p not in primes:
         # Unlike in is_left_truncatable, we can't check if p is prime by just
         # folding this check into the loop, since string[:0] returns the empty
         # string, not the full string
         return False
     p = str(p)
     for i in range(1, len(p)):
-        if not is_prime(int(p[:i])):
+        if int(p[:i]) not in primes:
             return False
     return True
 
