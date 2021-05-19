9feb69740eae95ca968d4faf8a9f52eb44e7c181
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-05-19 11:00:45 -0700

    40: Optimize space-efficient solution for speed a bit

--- 40--champernowne/space-efficient.py
+++ 40--champernowne/space-efficient.py
@@ -1,3 +1,12 @@
+# This solution is a bit of a hack: It depends on the fact that none of the
+# the target_indices given in the problem are "near" each other. It will not
+# work if target_indices includes indices near each other, i.e. indices that
+# are part of the same integer. For example, digits 14 and 15, marked with an
+# asterisk below, are "near" each other:
+#
+#                  **
+# 0.012345678901011121314...
+
 # 1-indexed
 target_indices = [
     1,
@@ -8,19 +17,22 @@ target_indices = [
     100_000,
     1_000_000,
 ]
+target = target_indices[0]
 
 i = 1
 n = 1
 result = 1
 
-while i <= max(target_indices):
+while True:
     inc = len(str(n))
 
-    # Possible optimization: Don't iterate through all of them, just the
-    # "next" one
-    for target in target_indices:
-        if target in range(i, i + inc):
-            result *= int(str(n)[target - i])
+    if target in range(i, i + inc):
+        result *= int(str(n)[target - i])
+        target_indices.pop(0)
+        try:
+            target = target_indices[0]
+        except IndexError:
+            break
 
     i += inc
     n += 1
