a465166056c930557a74f78f18d6e55cb3ecca50
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-06-04 23:20:28 -0700

    finish the problem

--- 031--coins/main.py
+++ 031--coins/main.py
@@ -1,16 +1,19 @@
 import functools
 
-coins = [1, 2, 5, 10, 20, 50, 100, 200]
+coins = [200, 100, 50, 20, 10, 5, 2, 1]
+# My intuition tells me to use this order, but I suppose it's not needed
 
-@functools.lru_cache(maxsize=None)
-def count(n, depth=0):
-    if n == 0:
+@functools.lru_cache(maxsize=None)  # Not strictly needed for this size input
+def count(n, coin_index=0):
+    """
+    Returns the number of ways to make n with only the given coin and smaller
+    """
+    if coin_index >= len(coins) or n < 0:
+        return 0
+    elif n == 0:
         return 1
-    result = 0
-    for coin in coins:
-        if coin <= n:
-            result += count(n - coin, depth+1)
-    print('  ' * depth, n, result)
-    return result
+    else:
+        return (count(n - coins[coin_index], coin_index)
+                + count(n, coin_index + 1))
 
-print(count(4))
+print(count(200))
