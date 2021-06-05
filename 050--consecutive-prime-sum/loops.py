662714de6f103dababa1ac702fe35d2ff3e04c37
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-06-05 00:49:11 -0700

    "optimization" -- does not actually run faster, but better order...
    ...means that you get the answer quickly (it just takes a while to
    verify it's definitely the correct answer)

--- 050--consecutive-prime-sum/loops.py
+++ 050--consecutive-prime-sum/loops.py
@@ -1,3 +1,16 @@
-    for stop in range(count + 1):
-        for start in range(0, stop):
+# same thing, in different orders
 
+primes = 'abcde'
+count = len(primes)
+for stop in range(count + 1):
+    for start in range(0, stop):
+        print(primes[start:stop])
+
+print()
+
+primes = 'abcde'
+count = len(primes)
+for length in range(1, count + 1):
+    for start in range(0, count - length + 1):
+        stop = start + length
+        print(primes[start:stop])
--- 050--consecutive-prime-sum/main.js
+++ 050--consecutive-prime-sum/main.js
@@ -5,18 +5,22 @@ function main() {
     .trim()
     .split('\n')
     .map(x => +x)
-    // .filter(x => x <= 1000);
+    .filter(x => x <= 1000000);
   const primesSet = new Set(primes);
   const prefixSums = getPrefixSums(primes);
   const count = primes.length;
   let maxLength = 0;
   let result = 0;
-  for (let stop = 0; stop <= count; stop++) {
-    for (let start = 0; start < stop; start++) {
+  for (let length = 1; length <= count; length++) {
+    const maxStart = count - length; // does not inlining this matter
+    for (let start = 0; start <= maxStart; start++) {
+      stop = start + length
       const mySum = sumRange(prefixSums, start, stop);
-      if (primesSet.has(mySum) && stop - start > maxLength) {
-        maxLength = stop - start;
+      if (primesSet.has(mySum)) {
+        maxLength = length;
         result = mySum;
+        console.log(maxLength, result);
+        break;
       }
     }
   }
