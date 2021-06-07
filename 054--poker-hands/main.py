3cee048a078c443a7dd1db776ca3aafb64a2bef6
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-06-06 23:00:11 -0700

    54: Ranks (no tiebreaks yet)

---
+++ 054--poker-hands/main.py
@@ -0,0 +1,74 @@
+import collections
+
+def get_value(card):
+    value = card[0]
+    try:
+        return int(value)
+    except ValueError:
+        return {
+            'T': 10,
+            'J': 11,
+            'Q': 12,
+            'K': 13,
+            'A': 14,
+        }[value]
+
+def get_suit(card):
+    return card[1]
+
+def get_rank(hand):
+    """
+    >>> get_rank('AS KS QS JS TS'.split())[1]
+    'Royal Flush'
+    >>> get_rank('KS QS JS TS 9S'.split())[1]
+    'Straight Flush'
+    >>> get_rank('8S 8C 8D 8H 3S'.split())[1]
+    'Four of a Kind'
+    >>> get_rank('8S 8C 8D 3H 3S'.split())[1]
+    'Full House'
+    >>> get_rank('2S QS JS TS 9S'.split())[1]
+    'Flush'
+    >>> get_rank('KD QS JS TS 9S'.split())[1]
+    'Straight'
+    >>> get_rank('8S 8C 8D 4H 3S'.split())[1]
+    'Three of a Kind'
+    >>> get_rank('8S 8C 4D 4H 3S'.split())[1]
+    'Two Pairs'
+    >>> get_rank('8S 8C 5D 4H 3S'.split())[1]
+    'One Pair'
+    >>> get_rank('8S 7C 5D 4H 3S'.split())[1]
+    'High Card'
+    """
+    suits = set(get_suit(card) for card in hand)
+    values = set(get_value(card) for card in hand)
+    is_flush = len(suits) == 1
+    is_straight = (len(values) == 5 and
+        min(values) + 4 == max(values))
+    kinds = get_kinds(hand)
+    kind_counts = [count for count, value in kinds]
+
+    if is_flush and values == {10, 11, 12, 13, 14}:
+        return (100, 'Royal Flush')
+    elif is_flush and is_straight:
+        return (90, 'Straight Flush')
+    elif kind_counts == [4, 1]:
+        return (80, 'Four of a Kind')
+    elif kind_counts == [3, 2]:
+        return (70, 'Full House')
+    elif is_flush:
+        return (60, 'Flush')
+    elif is_straight:
+        return (50, 'Straight')
+    elif kind_counts == [3, 1, 1]:
+        return (40, 'Three of a Kind')
+    elif kind_counts == [2, 2, 1]:
+        return (30, 'Two Pairs')
+    elif kind_counts == [2, 1, 1, 1]:
+        return (20, 'One Pair')
+    else:
+        assert kind_counts == [1]*5
+        return (10, 'High Card')
+
+def get_kinds(hand):
+    _ = ((count, value) for value, count in collections.Counter(get_value(card) for card in hand).items())
+    return list(sorted(_, reverse=True))
